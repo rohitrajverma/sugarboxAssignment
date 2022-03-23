5470601ef0beb36bdf8b1d0e5b5e145a9093bcd3
Rohitrajverma <rohitrajverma1611@gmail.com>
2022-03-23 21:59:40 +0530

    findmeOut changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sugarboxAssignment';
+  inputNum: number
+  result: any;
+
+
+  findMeOut(n: number): any {
+    n = +n
+    for (let i = 2; i < Infinity; i++) {
+      if (this.sumOfDigits(n * i) === n) {
+        return n * i;
+      }
+    }
+  }
+
+  callFun(n: number) {
+    this.result = this.findMeOut(n);
+  }
+  reset() {
+    if (!this.inputNum) {
+      this.result = '';
+    }
+  }
+  sumOfDigits(num: number): number {
+    let numStr: string = num.toString();
+    let newNum: string[] = numStr.split('');
+    return newNum.reduce((a, c) => a + parseInt(c), 0);
+  }
+
 }
