737de2f490b92ff61bb60d4c3f28f8b7b3ef952c
Rohitrajverma <rohitrajverma1611@gmail.com>
2022-03-24 00:44:15 +0530

    lint fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   inputNum: number
-  result: any;
+  result: number | string;
 
 
-  findMeOut(n: number): any {
+  findMeOut(n: number): number | string {
     n = +n
     for (let i = 2; i < Infinity; i++) {
       if (this.sumOfDigits(n * i) === n) {
         return n * i;
       }
     }
+    return "not available"
   }
 
   callFun(n: number) {
     this.result = this.findMeOut(n);
   }
+
   reset() {
     if (!this.inputNum) {
       this.result = '';
     }
   }
+
   sumOfDigits(num: number): number {
     let numStr: string = num.toString();
     let newNum: string[] = numStr.split('');
